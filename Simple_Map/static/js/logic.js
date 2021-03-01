670cc22a9a708cb8954e4a9a88b5dbbf0042583e
flowersmichael <74375396+flowersmichael@users.noreply.github.com>
2021-02-28 17:11:53 -0800

    majorAirports

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -25,10 +25,15 @@ let sanFranAirport =
 ]};
 
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport).addTo(map);
+L.geoJson(sanFranAirport, {
+   onEachFeature: function(feature, layer) {
+       console.log(layer);
+       layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+   }
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
